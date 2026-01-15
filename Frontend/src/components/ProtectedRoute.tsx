8911d21599ac69a61cf08106cf717dfef43f69b3
Gaurish Naik <dayanand.naik9809@gmail.com>
2026-01-16 00:04:00 +0530

    updated file

--- Frontend/src/components/ProtectedRoute.tsx
+++ Frontend/src/components/ProtectedRoute.tsx
@@ -18,7 +18,7 @@ function ProtectedRoute({ children }: ProtectedRouteProps) {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="min-h-screen bg-amber-50 flex items-center justify-center">
         <div className="inline-block animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
       </div>
     );
